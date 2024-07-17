3d1db7e4fa2c6f856158734466788da5b9229bd9
KamalAchalhi <kamal842010@hotmail.fr>
2024-07-17 11:34:45 +0200

    ..

--- script1.py
+++ script1.py
@@ -26,46 +26,43 @@ def etape1(nom_general_sujet, path_pattern, path_ouput, Nom_Atlas_anatomic_Patte
 
     ###COMMENTER LE MAXIMUM DE LIGNE POUR RENDRE CLAIRE
     debut = time.time()
-    files_atlas = tls.Parcours_dossier_only_data_match(path_des_atlas, Nom_Atlas_anatomic_Pattern)   #
-    tab_path_sujet = tls.recup_les_sujets(nom_general_sujet, pattern_sous_repertoire_by_sujet=path_pattern)  #
-    print(tab_path_sujet)
-    list_path_sujet_rot = [tls.creation_PATH_pour_fichier_swaper(sujet_path, path_ouput) for sujet_path in tab_path_sujet] #
+    files_atlas = tls.Parcours_dossier_only_data_match(path_des_atlas, Nom_Atlas_anatomic_Pattern)   #donne un repertoire contenant les atlas et nom qui caracterise tout les atlas que je cherche, il me renvoie une liste de tout les paths fichiers qui correspondent dedans
+    tab_path_sujet = tls.recup_les_sujets(nom_general_sujet, pattern_sous_repertoire_by_sujet=path_pattern)  # donne le repertoire qui contiens mes sujets anatomiques et le nom caracterisque d'un tel fichier
+    list_path_sujet_rot = [tls.creation_PATH_pour_fichier_swaper(sujet_path, path_ouput) for sujet_path in tab_path_sujet] # à parti des path des sujets anatomique obtenue en haut, je crée des path pour mes images sujets après rotation/transposition, j'y met le repertoire de sortie et le path de chaque sujet initial
     for path_sujet_rot, path_sujet in zip(list_path_sujet_rot, tab_path_sujet):
-        tls.SWAP_COPY_INFO_SAVE(path_sujet, path_sujet_rot)
-    tab_repertoire, tab_img_sujet = tls.path_abs_sujet_to_fichier_repertorie_sujet(list_path_sujet_rot) #
-    print(tab_img_sujet)
+        tls.SWAP_COPY_INFO_SAVE(path_sujet, path_sujet_rot)             # swapp l'image et la sauve dans le path associé crée précédement (je met en input le path de l'image initial et le repertoire output de sorti ici output1)
+    tab_repertoire, tab_img_sujet = tls.path_abs_sujet_to_fichier_repertorie_sujet(list_path_sujet_rot) # Pour chaque image swapé je sépare le nom du fichier et le nom du repertoire
 
-    criteres = ['MattesMutualInformation']
-    list_atlas_meilleur = list()
-    list_tranf_direc = []
-    list_tranf_inv = []
-    for sujet, repertoire in zip(tab_img_sujet, tab_repertoire):
-        bon_atlas, path_trf_direct, path_trf_inv = tls.recup_bon_atlas_avc_transfos(files_atlas, criteres, path_des_atlas, sujet, repertoire, "Rigid", "linear", file_transfo_direc, file_transfo_inv)
-        list_atlas_meilleur.append(bon_atlas)
-        list_tranf_direc.append(path_trf_direct)
-        list_tranf_inv.append(path_trf_inv)
+    criteres = ['MattesMutualInformation']      #Création d'une liste de critére pour la fonction ants de mesure de similarité, je choisis le MI qui est le plus pertinent
+    list_atlas_meilleur = list()   #création d'une liste pour être remplis par nom atlas qui maximise la similarité pour chaque sujet
+    list_tranf_direc = []       #création d'une liste pour contenir path des transfo recalage direct obtenu entre meilleur atlas et sujet associés
+    list_tranf_inv = []         #création d'une liste pour contenir path des transfo recalage inverse obtenu entre meilleur atlas et sujet associés
+    for sujet, repertoire in zip(tab_img_sujet, tab_repertoire):    #Je parcours chaque sujet de la liste
+        bon_atlas, path_trf_direct, path_trf_inv = tls.recup_bon_atlas_avc_transfos(files_atlas, criteres, path_des_atlas, sujet, repertoire, "Rigid", "linear", file_transfo_direc, file_transfo_inv)  # J'applique le traitement qui recale (type de recalage que j'ai imposé) l'image avec chaque atlas, mesure similarité que j'ai imposé en critére, selectionne meilleur atlas et renvoie le meilleur atlas et les transfos recalage associés
+        list_atlas_meilleur.append(bon_atlas) # Je recup le meilleur atlas pour le ième sujet parcourus dans cette boucle
+        list_tranf_direc.append(path_trf_direct) # recup trsfo recal pour ième sujet et meilleur atlas estimé
+        list_tranf_inv.append(path_trf_inv) #idem mais pour transfo inverse
         print(f"l'atlas qui maximise l'information mutuel est : {bon_atlas} pour {sujet}\n")
     fin = time.time()
     tps_excecution = fin - debut
     print(f"le temps d'exécution du programme est : {tps_excecution} secondes")
-    return list_atlas_meilleur, list_path_sujet_rot, list_tranf_direc, list_tranf_inv
+    return list_atlas_meilleur, list_path_sujet_rot, list_tranf_direc, list_tranf_inv       #Je retourne les éléments essentiel pour la suite, les meilleurs atlas, les path des sujets swapé, et les listes de transfo
 
 
 if __name__ == "__main__":
-    file_transfo_direc = r'/envau/work/meca/users/2024_Kamal/output/output_script1'
-    file_transfo_inv = r'/envau/work/meca/users/2024_Kamal/output/output_script2'
-    path_variables = "/envau/work/meca/users/2024_Kamal/2024_stage_Kamal/variables"
-    nom_general_sujet = r'^sub-00\d+\_ses-00\d+\_acq-haste_rec-nesvor_desc-aligned_T2w.nii.gz'
-    path_pattern = r'/envau/work/meca/users/2024_Kamal/real_data/lastest_nesvor/sub-00\d+\/ses-00\d+\/haste/default_reconst'
-    all_sujets_path = "/envau/work/meca/users/2024_Kamal/real_data/lastest_nesvor"
-    path_output = "/envau/work/meca/users/2024_Kamal/output/output_script1"
-    Nom_Atlas_anatomic_Pattern = r'^STA\d+\.nii.gz'
-    path_des_atlas = "/envau/work/meca/users/2024_Kamal/Sym_Hemi_atlas/Fetal_atlas_gholipour/T2"
-    path_output_avec_mask = "/envau/work/meca/users/2024_Kamal/output/Output_script1_avec_mask"
-    # chemin_vers_mask = np.load(os.path.join(path_variables,"chemin_vers_mask.npy"))
+    file_transfo_direc = r'/envau/work/meca/users/2024_Kamal/output/output_script1'  #J'y sauve mes transformations direct de recalage
+    file_transfo_inv = r'/envau/work/meca/users/2024_Kamal/output/output_script2' # J'y sauve mes transformations inverse de recalage
+    path_variables = "/envau/work/meca/users/2024_Kamal/2024_stage_Kamal/variables" # J'y sauve mes listes de path retourner par etape1
+    nom_general_sujet = r'^sub-00\d+\_ses-00\d+\_acq-haste_rec-nesvor_desc-aligned_T2w.nii.gz' #format nom sujet anatomique
+    path_pattern = r'/envau/work/meca/users/2024_Kamal/real_data/lastest_nesvor/sub-00\d+\/ses-00\d+\/haste/default_reconst' #format path repertoire sujet anatomique
+    all_sujets_path = "/envau/work/meca/users/2024_Kamal/real_data/lastest_nesvor" # Path repertoire sujet segmenté
+    path_output = "/envau/work/meca/users/2024_Kamal/output/output_script1" # J'y sauve mes image après traitement de swap
+    Nom_Atlas_anatomic_Pattern = r'^STA\d+\.nii.gz' #format nom atlas anatomique
+    path_des_atlas = "/envau/work/meca/users/2024_Kamal/Sym_Hemi_atlas/Fetal_atlas_gholipour/T2" #path repertorie atlas anatomique
+
     list_atlas_meilleur, list_path_sujet_rot, list_tranf_direc, list_tranf_inv = etape1(nom_general_sujet, path_pattern, path_output, Nom_Atlas_anatomic_Pattern, path_des_atlas, file_transfo_direc, file_transfo_inv)
 
-    print(list_atlas_meilleur, list_tranf_inv)
+    # Je sauve chaque liste de nom ou path dans un fichier numpy dans le repertoire dediés aux variables
     np.save(os.path.join(path_variables, "list_atlas_meilleur.npy"), list_atlas_meilleur, allow_pickle='False')
     np.save(os.path.join(path_variables, "list_path_sujet_rot.npy"), list_path_sujet_rot, allow_pickle='False')
     np.save(os.path.join(path_variables, "list_tranf_direc.npy"), list_tranf_direc, allow_pickle='False')
--- script2.py
+++ script2.py
@@ -9,42 +9,33 @@ import ants
 import tools as tls
 
 
-def etape2(path_des_atlas_hemi_seg, list_atlas_meilleur,tab_path_sujet,list_tranf_inv,path_output_repertoire):
+def etape2(path_des_atlas_hemi_seg, list_atlas_meilleur, list_path_sujet_rot, list_tranf_inv, path_output_repertoire):
     """
 
     :param path_des_atlas_hemi_seg: Chemins des atlas segmenté par hemisphères RL
     :param list_atlas_meilleur: Listes des chemins pour chaque atlas mieux adaptés à un sujet données
-    :param tab_img_sujet_rot: Listes des noms de chaques images de sujets anatomiques
+    :param list_path_sujet_rot: Listes chemin vers repertoire où on était sauvé les iamges de sujet anatomiques après "swaping" du script1
     :param list_tranf_inv: Listes des chemins vers les transformations de recalage inverse obtenue entre chaque sujet et son meilleur atlas
-    :param path_repertoire_sujet_rot: Listes chemin vers repertoire où on était sauvé les iamges de sujet anatomiques après "swaping" du script1
     :param path_output_repertoire: Chemin de sauvegarde des images après traitement réalisé par script2
     :return: AtlasLR_rec_dans_sub_space : Atlas segmenté LR recalé dans l'espace sujet associès
     """
     debut = time.time()
-    #On cherche à recaler l'atlas sur l'image (une inversion du recalage), nous utilisons cette fois l'atlas binar
-    #NOM des ATLAS Binairs
-    print(tab_path_sujet)
-    tab_repertoire, tab_img_sujet = tls.path_abs_sujet_to_fichier_repertorie_sujet(tab_path_sujet)
-    tls.creation_data_frame_sujet_by_best_atlas(tab_img_sujet, list_atlas_meilleur)
-    les_atlas_binary = []
-    list_num = tls.extraction_numero_atlas(list_atlas_meilleur)
-    for num in list_num:
-        les_atlas_binary.append(f'STA{num}_all_reg_LR_dilM.nii.gz')
-    AtlasRL_rec_dans_sub_space= []  # liste des...
-    for sujet, repertoire,  atlas_binar, warp in zip(tab_img_sujet, tab_repertoire, les_atlas_binary, list_tranf_inv):
-        Sujet_fixe = ants.image_read(os.path.join(repertoire, sujet))
-        Atlas_binary = ants.image_read(os.path.join(path_des_atlas_hemi_seg, atlas_binar))
-        transfo = warp + '_Inverse_0GenericAffine.mat'
-        print(transfo)
-        Atlas_binary_warped = ants.apply_transforms(Sujet_fixe, Atlas_binary,  transformlist=transfo, direction = 'fwd', interpolator="nearestNeighbor")
-        print(type(Atlas_binary_warped))
-        # Atlas_binary_warped.set_spacing(Sujet_fixe.spacing)
-        # Atlas_binary_warped.set_origin(Sujet_fixe.origin)
-        # Atlas_binary_warped .set_direction(Atlas_binary.direction)
-        path_atlas_binary_warped = tls.creation_chemin_nom_img(path_output_repertoire, sujet, atlas_binar)
-        AtlasRL_rec_dans_sub_space.append(path_atlas_binary_warped)
-        # nib.save(Atlas_binary_warped_nifti, path_atlas_binary_warped)
-        ants.image_write(Atlas_binary_warped, path_atlas_binary_warped)
+
+    tab_repertoire, tab_img_sujet = tls.path_abs_sujet_to_fichier_repertorie_sujet(list_path_sujet_rot)  #Separation path repertoire du nom de l'image
+    tls.creation_data_frame_sujet_by_best_atlas(tab_img_sujet, list_atlas_meilleur)  #Creation tableau panda contenant pour chaque sujet son meilleur atlas associés
+    les_atlas_binary = [] #liste qui recuperera les path des atlas hemisphère binaire
+    list_num = tls.extraction_numero_atlas(list_atlas_meilleur) # récupération pour chaque atlas adapté du numero/age de l'atlas dans une liste
+    for num in list_num:  #parcours de chaque age
+        les_atlas_binary.append(f'STA{num}_all_reg_LR_dilM.nii.gz') # rassemble format nom des atlas hemisphere binaire et de l'age recupere, obtention liste des atlas binaire gauche droite adapté
+    AtlasRL_rec_dans_sub_space= []  # liste pour conserver les chemins des atlas segmentés LR recaler dans l'espace du sujet
+    for sujet, repertoire,  atlas_binar, warp in zip(tab_img_sujet, tab_repertoire, les_atlas_binary, list_tranf_inv): # parcours les sujet swaper, les atlas binar RL et les transfo inverse associées
+        Sujet_fixe = ants.image_read(os.path.join(repertoire, sujet)) #recup l'image sujet swap en format ants
+        Atlas_binary = ants.image_read(os.path.join(path_des_atlas_hemi_seg, atlas_binar))  # recup atlas binar RL grâce au nom formé ligne29 et path athlas binar RL
+        transfo = warp + '_Inverse_0GenericAffine.mat' # étant donné que ma fct enregistre les fonction de recalage dans la list sans prendre en compte ce sufixe, il doit être rajouté
+        Atlas_binary_warped = ants.apply_transforms(Sujet_fixe, Atlas_binary,  transformlist=transfo, direction = 'fwd', interpolator="nearestNeighbor") #Application de la trf inverse sur l'atlas binar RL adapté vers le ième sujet
+        path_atlas_binary_warped = tls.creation_chemin_nom_img(path_output_repertoire, sujet, atlas_binar) #creation de path de l'atlas binar RL apres son recalage dans espace sujet
+        AtlasRL_rec_dans_sub_space.append(path_atlas_binary_warped) #rajoute à liste des path des atlas RL apres rec
+        ants.image_write(Atlas_binary_warped, path_atlas_binary_warped) #Enregistre l'image obtenue dans le path crée
     fin = time.time()
     tps_excecution = fin - debut
     print(f"le temps d'exécution du programme est : {tps_excecution} secondes")
@@ -52,16 +43,16 @@ def etape2(path_des_atlas_hemi_seg, list_atlas_meilleur,tab_path_sujet,list_tran
 
 
 if __name__ == "__main__":
-    nom_general_sujet = r'^sub-00\d+\_ses-00\d+\_acq-haste_rec-nesvor_desc-aligned_T2w.nii.gz'
-    path_pattern = r'/envau/work/meca/users/2024_Kamal/real_data/lastest_nesvor/sub-00\d+\/ses-00\d+\/haste/default_reconst'
-    path_des_atlas_hemi_seg = r'/envau/work/meca/users/2024_Kamal/Sym_Hemi_atlas'
+    nom_general_sujet = r'^sub-00\d+\_ses-00\d+\_acq-haste_rec-nesvor_desc-aligned_T2w.nii.gz' #format nom sujet anatomique
+    path_pattern = r'/envau/work/meca/users/2024_Kamal/real_data/lastest_nesvor/sub-00\d+\/ses-00\d+\/haste/default_reconst' #format path repertoire sujet anatomique
+    path_des_atlas_hemi_seg = r'/envau/work/meca/users/2024_Kamal/Sym_Hemi_atlas' #path des atlas binaire par hemisphère
     path_variables = "/envau/work/meca/users/2024_Kamal/2024_stage_Kamal/variables"
     path_repertoire_sujet_rot = "/envau/work/meca/users/2024_Kamal/output/output_script1"
-    path_output_repertoire = "/envau/work/meca/users/2024_Kamal/output/output_script2"
-    list_atlas_meilleur = np.load(os.path.join(path_variables, "list_atlas_meilleur.npy"))
-    tab_path_sujet = np.load(os.path.join(path_variables, "list_path_sujet_rot.npy"))
+    path_output_repertoire = "/envau/work/meca/users/2024_Kamal/output/output_script2" #path du repertoire pour sauver les atlas RL apres recalage
+    list_atlas_meilleur = np.load(os.path.join(path_variables, "list_atlas_meilleur.npy"))   #recup list des meilleurs atlas
+    list_path_sujet_rot = np.load(os.path.join(path_variables, "list_path_sujet_rot.npy"))
     list_tranf_direc =  np.load(os.path.join(path_variables, "list_tranf_direc.npy"))
     list_tranf_inv = np.load(os.path.join(path_variables, "list_tranf_inv.npy"))
 
-    AtlasRL_rec_dans_sub_space = etape2(path_des_atlas_hemi_seg, list_atlas_meilleur, tab_path_sujet, list_tranf_inv, path_output_repertoire)
+    AtlasRL_rec_dans_sub_space = etape2(path_des_atlas_hemi_seg, list_atlas_meilleur, list_path_sujet_rot, list_tranf_inv, path_output_repertoire)
     np.save(os.path.join(path_variables, "AtlasRL_rec_dans_sub_space.npy"), AtlasRL_rec_dans_sub_space, allow_pickle='False')
